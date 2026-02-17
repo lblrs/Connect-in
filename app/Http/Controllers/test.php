91f25cfca55d907d597e511b67873b6064a1f306
Mohammad <mohammad-haroon.masah@epitech.eu>
2026-02-17 14:12:36 +0100

    final: registration logic completed

---
+++ app/Http/Controllers/test.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Validator; 
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required | string | max:255',
+            'last_name' => 'required | string | max:255',
+            'email' => 'required | string | email | max:255 | unique:users',
+            'password' => 'required | string | min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully registered!',
+            'user' => $user
+        ], 201);
+    }
+}
