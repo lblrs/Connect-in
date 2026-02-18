bb4d685f0bf63abee25e440776ae80201a11cd6f
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-02-18 12:49:19 +0100

    Add register logic

--- connect_in/src/App.js
+++ connect_in/src/App.js
@@ -1,13 +1,25 @@
-import Register from "./pages/Register";
+import logo from './logo.svg';
+import './App.css';
 
-function App () {
+function App() {
   return (
-    <>
-    
-    <Register />
-
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
